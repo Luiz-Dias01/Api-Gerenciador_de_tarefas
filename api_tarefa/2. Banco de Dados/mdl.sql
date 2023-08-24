cb0daa380037760b40915871b0981340a81e50fe
Luiz-Dias01 <142491324+Luiz-Dias01@users.noreply.github.com>
2023-08-24 00:01:41 -0300

    Update dml

--- api_tarefa/2. Banco de Dados/mdl.sql
+++ api_tarefa/2. Banco de Dados/mdl.sql
@@ -1,22 +1,22 @@
 -- Inserir Registros
 INSERT INTO tb_tarefas (ds_tarefas, nr_ordem, bt_finalizado, dt_cadastro)
-		VALUES ROW ("Criar API de tarefas", 1, false, "2023-08-24");
+		VALUES ROW ("?", ?, ?, "?");
         
 -- Buscar Todos
 SELECT * FROM tb_tarefas;
 
 -- Buscar Tarefas Finalizadas
 SELECT * FROM tb_tarefas
-	WHERE bt_finalizado = true;
+	WHERE bt_finalizado = ?;
     
 -- Alterar Tarefas 
 UPDATE tb_tarefas
-	SET ds_tarefas = "",
-		nr_ordem = 1,
-        bt_finalizado = true,
-        dt_cadastro = "2023-08-24"
-			WHERE id_tarefas = 1;
+	SET ds_tarefas = "?",
+		nr_ordem = ?,
+        bt_finalizado = ?,
+        dt_cadastro = "?"
+			WHERE id_tarefas = ?;
             
 -- Deletar Tarefas
 DELETE FROM tb_tarefas
-	WHERE id_tarefas = 1;
+	WHERE id_tarefas = ?;
